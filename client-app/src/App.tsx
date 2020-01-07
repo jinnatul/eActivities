8288fdd2ded567729f656ff5b94db095f2cf011f
jinnatul <morolswediu@gmail.com>
2020-01-07 22:53:14 +0600

    react state use for DemoList

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -3,22 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    values: []
+  };
+
+  componentDidMount() {
+    this.setState({
+      values: [{id: 1, name: "Morol"}, {id: 2, name: "Farhana"}]
+    })
+  }
+
   render () {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          
+          <ul>
+            {this.state.values.map((value: any) => (
+              <li>{value.name}</li>
+            ))}
+          </ul>
         </header>
       </div>
     );
