ac4cac98508630a3f51c6ce8003bee72b983c926
jinnatul <morolswediu@gmail.com>
2020-01-10 01:29:15 +0600

    initialing Form with each Activity data

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -35,7 +35,12 @@ export const ActivityDashboard: React.FC<IProps> = ({
                         setSelectedActivity={setSelectedActivity}
                     />
                 )}
-                {editMode && <ActivityForm setEditMode={setEditMode}/>}
+                {editMode && (
+                    <ActivityForm 
+                        setEditMode={setEditMode} 
+                        activity={selectedActivity!} 
+                    />
+                )}
             </Grid.Column>
         </Grid>
     );
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,20 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Segment, Form, Button } from 'semantic-ui-react'
+import { IActivity } from '../../../app/model/activity'
 
 interface IProps {
     setEditMode: (editMode: boolean) => void;
+    activity: IActivity
 }
 
-export const ActivityForm: React.FC<IProps> = ({setEditMode}) => {
+export const ActivityForm: React.FC<IProps> = ({setEditMode, activity: initialeFormState}) => {
+    
+    const initializeForm = () => {
+        if(initialeFormState) return initialeFormState;
+        else {
+            return {
+                id: '',
+                title: '',
+                description: '',
+                category: '',
+                date: '',
+                city: '',
+                venue: ''
+            };
+        }
+    };
+
+    const [activity, setActivity] = useState<IActivity>(initializeForm)
+    
     return (
         <Segment clearing>
             <Form>
-                <Form.Input placeholder='Title' />
-                <Form.TextArea rows={2} placeholder='Description' />
-                <Form.Input placeholder='Category' />
-                <Form.Input type='date' placeholder='Date' />
-                <Form.Input placeholder='City' />
-                <Form.Input placeholder='Venue' />
+                <Form.Input placeholder='Title' value={activity.title}/>
+                <Form.TextArea rows={2} placeholder='Description' value={activity.description}/>
+                <Form.Input placeholder='Category' value={activity.category}/>
+                <Form.Input type='date' placeholder='Date' value={activity.date}/>
+                <Form.Input placeholder='City' value={activity.city}/>
+                <Form.Input placeholder='Venue' value={activity.venue}/>
                 <Button floated='right' positive type='submit' content='Submit' />
                 <Button 
                     onClick={() => setEditMode(false)} 
