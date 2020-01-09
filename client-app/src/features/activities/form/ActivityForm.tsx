84bcd398eb6622cecc0c9ef301b0d7cc99cc8f32
jinnatul <morolswediu@gmail.com>
2020-01-09 22:54:12 +0600

    adding activity Form component

---
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Segment, Form } from 'semantic-ui-react'
+
+export const ActivityForm = () => {
+    return (
+        <Segment>
+            <Form>
+                <Form.Input placeholder='Title' />
+                <Form.TextArea rows={2} placeholder='Description' />
+                <Form.Input placeholder='Category' />
+                <Form.Input type='date' placeholder='Date' />
+                <Form.Input placeholder='City' />
+                <Form.Input placeholder='Venue' />
+            </Form>
+        </Segment>
+    )
+}
