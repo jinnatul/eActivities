3d9dedca6d735a703fc8bc101e6bca5ac0f79539
jinnatul <morolswediu@gmail.com>
2020-01-09 22:27:26 +0600

    creating the activityList

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,20 +1,17 @@
 import React from 'react'
 import { Grid, List } from 'semantic-ui-react'
 import { IActivity } from '../../../app/model/activity'
+import { ActivityList } from './ActivityList'
 
 interface IProps {
     activities: IActivity[]
 }
 
-export const ActivityDashboard: React.FC<IProps> = (props) => {
+export const ActivityDashboard: React.FC<IProps> = ({activities}) => {
     return (
         <Grid>
             <Grid.Column width={10}>
-               <List>
-                   {props.activities.map((activity) => (
-                     <List.Item key={activity.id}>{activity.title}</List.Item>
-                  ))}
-                </List>
+            <ActivityList activities={activities}/>
             </Grid.Column>
         </Grid>
     )
