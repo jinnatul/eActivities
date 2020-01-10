b941ebd9dc1cb3e74b6186a8c599bbea790a1038
jinnatul <morolswediu@gmail.com>
2020-01-11 01:35:08 +0600

    creating an activity

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -22,9 +22,11 @@ const App = () => {
   }
 
   const handleCreateActivity = (activity: IActivity) => {
-    setActivities([...activities, activity])
-    setSelectedActivity(activity);
-    setEditMode(false);
+    agent.Activities.create(activity).then(() => {
+      setActivities([...activities, activity])
+      setSelectedActivity(activity);
+      setEditMode(false);
+    })
   }
 
   const handleEditActivity = (activity: IActivity) => {
