e757c9d0562274d2a45694e374732b674693d51b
jinnatul <morolswediu@gmail.com>
2020-01-11 21:24:35 +0600

    async await add loadActivities method

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -9,16 +9,21 @@ class ActivityStore {
     @observable loadingInitial = false;
     @observable editMode = false;
 
-    @action loadActivities = () => {
+    @action loadActivities = async () => {
         this.loadingInitial = true;
-        agent.Activities.list()
-        .then(activities => {
+        try {
+            const activities = await agent.Activities.list();
             activities.forEach(activity => {
-            activity.date = activity.date.split('.')[0];
-            this.activities.push(activity);
-        })
-       }).finally(() => this.loadingInitial = false);
-    }
+                activity.date = activity.date.split('.')[0];
+                this.activities.push(activity);
+            });
+            this.loadingInitial = false;
+            
+        } catch (error) {
+            console.log(error);
+            this.loadingInitial = false;
+        }
+    };
 
     @action selectActivity = (id: string) => {
         this.selectedActivity = this.activities.find(a => a.id === id);
