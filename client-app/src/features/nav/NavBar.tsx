6079676019b808a670d5beefa6b950c0b58b1582
jinnatul <morolswediu@gmail.com>
2020-01-12 18:04:34 +0600

    adding NavLink

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { Menu, Container, Button } from 'semantic-ui-react'
 import ActivityStore from '../../app/stores/activityStore';
 import { observer } from 'mobx-react-lite';
+import { NavLink } from 'react-router-dom';
 
 const NavBar: React.FC = () => {
     
@@ -9,13 +10,14 @@ const NavBar: React.FC = () => {
     return (
       <Menu fixed='top' inverted>
         <Container>
-            <Menu.Item header>
+            <Menu.Item header as={NavLink} exact to='/'>
                 <img src='/items/logo.png' alt='logo' style={{marginRight: '10px'}}/>
                 eActivities
             </Menu.Item>
-            <Menu.Item name='Activities'/>
+            <Menu.Item name='Activities' as={NavLink} to='/activities'/>
             <Menu.Item>
                 <Button 
+                   as={NavLink} to='/createActivity'
                   onClick={activityStore.openCreateForm} 
                   positive content='Create Activity' 
                 />
