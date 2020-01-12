ad48e2cbb6ed70bc42a9a96371127bf012eff92f
jinnatul <morolswediu@gmail.com>
2020-01-12 18:09:46 +0600

    adding Details Link

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -2,11 +2,12 @@ import React, { useContext } from 'react'
 import { Item, Button, Label, Segment } from 'semantic-ui-react'
 import { observer } from 'mobx-react-lite';
 import ActivityStore from '../../../app/stores/activityStore';
+import { Link } from 'react-router-dom';
 
 const ActivityList: React.FC = () => {
 
     const activityStore = useContext(ActivityStore);
-    const {activitiesByDate, selectActivity, deleteActivity, submitting, target} 
+    const {activitiesByDate, deleteActivity, submitting, target} 
         = activityStore;
 
     return (
@@ -23,7 +24,7 @@ const ActivityList: React.FC = () => {
                             </Item.Description>
                             <Item.Extra>
                                 <Button 
-                                    onClick={() => selectActivity(activity.id)} 
+                                    as={Link} to={`/activity/${activity.id}`}
                                     floated='right' 
                                     content='View' 
                                     color='blue' 
