0336c4759b02ee40c64ac9711163a6f57cf3aabf
jinnatul <morolswediu@gmail.com>
2020-01-12 17:58:10 +0600

    setting up our Routes App.tsx

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,6 +5,9 @@ import ActivityDashboard  from '../../features/activities/dashboard/ActivityDash
 import { LoadingComponent } from './LoadingComponent';
 import ActivityStore from '../../app/stores/activityStore';
 import { observer } from 'mobx-react-lite';
+import { Route } from 'react-router-dom';
+import HomePage from '../../features/home/HomePage';
+import ActivityForm from '../../features/activities/form/ActivityForm';
 
 const App = () => {
   
@@ -20,7 +23,9 @@ const App = () => {
     <Fragment>
       <NavBar />
       <Container style={{marginTop: '7em'}}>
-        <ActivityDashboard />
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/activities' component={ActivityDashboard}/>
+        <Route path='/createActivity' component={ActivityForm}/>
       </Container>
     </Fragment>
   );
---
+++ client-app/src/features/home/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'semantic-ui-react'
+
+const HomePage = () => {
+    return (
+        <Container style={{marginTop: '7em'}}>
+            <h1>From Route</h1>
+        </Container>
+    )
+}
+
+export default HomePage;
