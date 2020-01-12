25b5a695ea395d8f38500861e5875f3cb302a6da
jinnatul <morolswediu@gmail.com>
2020-01-13 03:17:29 +0600

    styling activity list Items

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,42 +1,42 @@
-import React, { useContext } from 'react'
-import { Item, Button, Label } from 'semantic-ui-react';
+import React from 'react'
+import { Item, Button, Segment, Icon } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
-import  ActivityStore  from '../../../app/stores/activityStore';
 import { IActivity } from '../../../app/model/activity';
 
 const ActivityListItem: React.FC<{activity: IActivity}> = ({activity}) => {
 
-    const activityStore = useContext(ActivityStore);
-    const {deleteActivity, submitting, target} 
-        = activityStore;
     return (
-        <Item key={activity.id}>
-            <Item.Content>
-                <Item.Header as='a'>{activity.title}</Item.Header>
-                <Item.Meta>{activity.date}</Item.Meta>
-                <Item.Description>
-                    <div>{activity.description}</div>
-                    <div>{activity.city}, {activity.venue}</div>
-                </Item.Description>
-                <Item.Extra>
-                    <Button 
-                        as={Link} to={`/activities/${activity.id}`}
-                        floated='right' 
-                        content='View' 
-                        color='blue' 
-                    />
-                    <Button 
-                        name={activity.id}
-                        loading={target === activity.id && submitting}
-                        onClick={(e) => deleteActivity(e, activity.id)} 
-                        floated='right' 
-                        content='Delete' 
-                        color='red' 
-                    />
-                    <Label basic content={activity.category} />
-                </Item.Extra>
-            </Item.Content>
-        </Item>
+        <Segment.Group>
+            <Segment>
+                <Item.Group>
+                    <Item>
+                        <Item.Image size='tiny' circular src='/items/user.png' />
+                        <Item.Content>
+                            <Item.Header as='a'>{activity.title}</Item.Header>
+                            <Item.Description>
+                                Hosted by Morol
+                            </Item.Description>
+                        </Item.Content>
+                    </Item>
+                </Item.Group>
+            </Segment>
+            <Segment>
+                <Icon name='clock' />{activity.date}
+                <Icon name='marker' />{activity.venue},{activity.city}
+            </Segment>
+            <Segment secondary>
+                Attendees will go here
+            </Segment>
+            <Segment clearing>
+                <span>{activity.description}</span>
+                <Button 
+                    as={Link} to={`/activities/${activity.id}`}
+                    floated='right' 
+                    content='View' 
+                    color='blue' 
+                />
+            </Segment>
+        </Segment.Group>
     )
 }
 
