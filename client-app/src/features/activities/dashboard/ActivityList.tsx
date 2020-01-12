9c6ac7127d06b1c01638cd76fa95689f7630d59d
jinnatul <morolswediu@gmail.com>
2020-01-13 02:20:00 +0600

    styling the activity list

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -3,6 +3,7 @@ import { Item, Button, Label, Segment } from 'semantic-ui-react'
 import { observer } from 'mobx-react-lite';
 import ActivityStore from '../../../app/stores/activityStore';
 import { Link } from 'react-router-dom';
+import ActivityListItem from './ActivityListItem';
 
 const ActivityList: React.FC = () => {
 
@@ -14,33 +15,7 @@ const ActivityList: React.FC = () => {
         <Segment clearing>
             <Item.Group divided>
                 {activitiesByDate.map(activity => (
-                    <Item key={activity.id}>
-                        <Item.Content>
-                            <Item.Header as='a'>{activity.title}</Item.Header>
-                            <Item.Meta>{activity.date}</Item.Meta>
-                            <Item.Description>
-                                <div>{activity.description}</div>
-                                <div>{activity.city}, {activity.venue}</div>
-                            </Item.Description>
-                            <Item.Extra>
-                                <Button 
-                                    as={Link} to={`/activities/${activity.id}`}
-                                    floated='right' 
-                                    content='View' 
-                                    color='blue' 
-                                />
-                                <Button 
-                                    name={activity.id}
-                                    loading={target === activity.id && submitting}
-                                    onClick={(e) => deleteActivity(e, activity.id)} 
-                                    floated='right' 
-                                    content='Delete' 
-                                    color='red' 
-                                />
-                                <Label basic content={activity.category} />
-                            </Item.Extra>
-                        </Item.Content>
-                    </Item>
+                    <ActivityListItem key={activity.id} activity={activity}/>
                 ))}
             </Item.Group>
         </Segment>
---
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -0,0 +1,43 @@
+import React, { useContext } from 'react'
+import { Item, Button, Label } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import  ActivityStore  from '../../../app/stores/activityStore';
+import { IActivity } from '../../../app/model/activity';
+
+const ActivityListItem: React.FC<{activity: IActivity}> = ({activity}) => {
+
+    const activityStore = useContext(ActivityStore);
+    const {activitiesByDate, deleteActivity, submitting, target} 
+        = activityStore;
+    return (
+        <Item key={activity.id}>
+            <Item.Content>
+                <Item.Header as='a'>{activity.title}</Item.Header>
+                <Item.Meta>{activity.date}</Item.Meta>
+                <Item.Description>
+                    <div>{activity.description}</div>
+                    <div>{activity.city}, {activity.venue}</div>
+                </Item.Description>
+                <Item.Extra>
+                    <Button 
+                        as={Link} to={`/activities/${activity.id}`}
+                        floated='right' 
+                        content='View' 
+                        color='blue' 
+                    />
+                    <Button 
+                        name={activity.id}
+                        loading={target === activity.id && submitting}
+                        onClick={(e) => deleteActivity(e, activity.id)} 
+                        floated='right' 
+                        content='Delete' 
+                        color='red' 
+                    />
+                    <Label basic content={activity.category} />
+                </Item.Extra>
+            </Item.Content>
+        </Item>
+    )
+}
+
+export default ActivityListItem;
