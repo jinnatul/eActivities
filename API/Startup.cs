d2875c1f653907241c349250a1dc7f392405e605
jinnatul <morolswediu@gmail.com>
2020-01-16 01:23:16 +0600

    using our error handling Middleware (Delete)

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,4 @@
+using API.MiddleWare;
 using Application.Activities;
 using FluentValidation.AspNetCore;
 using MediatR;
@@ -43,9 +44,11 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ErrorHandlingMiddleware>();
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                //app.UseDeveloperExceptionPage();
+                
             }
             else 
             {
