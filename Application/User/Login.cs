320a3006de965cfc49f40657ca3f5768cb125d0f
jinnatul <morolswediu@gmail.com>
2020-01-28 00:39:11 +0600

    retuningjwt token login time

--- Application/User/Login.cs
+++ Application/User/Login.cs
@@ -2,6 +2,7 @@ using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Errors;
+using Application.Interfaces;
 using Domain;
 using FluentValidation;
 using MediatR;
@@ -31,9 +32,12 @@ namespace Application.User
         {
             private readonly UserManager<AppUser> _userManager;
             private readonly SignInManager<AppUser> _signInManager;
+            private readonly IJwtGenerator _jwtGenerator;
             public Handler(UserManager<AppUser> userManager, 
-                SignInManager<AppUser> signInManager)
+                SignInManager<AppUser> signInManager, 
+                IJwtGenerator jwtGenerator)
             {
+                _jwtGenerator = jwtGenerator;
                 _signInManager = signInManager;
                 _userManager = userManager;
             }
@@ -42,24 +46,24 @@ namespace Application.User
                 CancellationToken cancellationToken)
             {
                 var user = await _userManager.FindByEmailAsync(request.Email);
-                if(user == null)
+                if (user == null)
                     throw new RestException(HttpStatusCode.Unauthorized);
-                
-                var result = await _signInManager.CheckPasswordSignInAsync(user, 
+
+                var result = await _signInManager.CheckPasswordSignInAsync(user,
                     request.Password, false);
-                
-                if(result.Succeeded) 
+
+                if (result.Succeeded)
                 {
                     return new User
                     {
                         DisplayName = user.DisplayName,
-                        Token = "This will be a token",
+                        Token = _jwtGenerator.CreateToken(user),
                         UserName = user.UserName,
                         Image = null
                     };
                 }
-                    
-                
+
+
                 throw new RestException(HttpStatusCode.Unauthorized);
             }
         }
