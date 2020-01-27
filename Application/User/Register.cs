e5947fdc44ea44725fe5979cb3014d93d8e82ef7
jinnatul <morolswediu@gmail.com>
2020-01-28 02:33:59 +0600

    adding register handler

---
+++ Application/User/Register.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Errors;
+using Application.Interfaces;
+using Domain;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.User
+{
+    public class Register
+    {
+        public class Command : IRequest<User>
+        {
+            public string DisplayName { get; set; }
+            public string UserName { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
+
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.DisplayName).NotEmpty();
+                RuleFor(x => x.UserName).NotEmpty();
+                RuleFor(x => x.Email).NotEmpty();
+                RuleFor(x => x.Password).NotEmpty();
+            }
+        }
+
+        public class Handler : IRequestHandler<Command, User>
+        {
+            private readonly DataContext _context;
+            private readonly UserManager<AppUser> _userManager;
+            private readonly IJwtGenerator _jwtGenerator;
+            public Handler(DataContext context, 
+                UserManager<AppUser> userManager, 
+                IJwtGenerator jwtGenerator)
+            {
+                _jwtGenerator = jwtGenerator;
+                _userManager = userManager;
+                _context = context;
+            }
+
+            public async Task<User> Handle(Command request, CancellationToken cancellationToken)
+            {
+                if(await _context.Users.Where(x => x.Email == request.Email).AnyAsync())
+                    throw new RestException(HttpStatusCode.BadRequest, 
+                        new { Email = "Email already taken!"});
+                
+                if(await _context.Users.Where(x => x.UserName == request.UserName).AnyAsync())
+                    throw new RestException(HttpStatusCode.BadRequest, 
+                        new { UserName = "UserName already taken!"});
+
+                var user = new AppUser 
+                {
+                    DisplayName = request.DisplayName,
+                    Email = request.Email,
+                    UserName = request.UserName
+                };
+
+                var result = await _userManager.CreateAsync(user, request.Password);
+
+                if (result.Succeeded) 
+                {
+                    return new User
+                    {
+                        DisplayName = user.DisplayName,
+                        Token = _jwtGenerator.CreateToken(user),
+                        UserName = user.UserName,
+                        Image = null
+                    };
+                }
+
+                throw new Exception("Problem creating user!");
+            }
+        }
+    }
+}
