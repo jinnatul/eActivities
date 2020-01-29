ac21c161ede540953ec37c68da3811d90ade550e
jinnatul <morolswediu@gmail.com>
2020-01-29 23:21:03 +0600

    adding home page and navbar with info

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useContext, Fragment } from 'react';
 import { Container, Segment, Header, Button, Image } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import { RootStoreContext } from '../../app/stores/rootStore';
 
 const HomePage = () => {
+    const rootStore = useContext(RootStoreContext);
+    const { isLoggedIn, user} = rootStore.userStore;
+
     return (
         <Segment inverted textAlign='center' vertical className='masthead' >
             <Container text>
@@ -10,10 +14,25 @@ const HomePage = () => {
                     <Image size='massive' src='/items/logo.png' alt='logo' style={{marginBottom: 12}}/>
                     eActivities
                 </Header>
-                <Header as='h2' inverted content='Welcome to eActivities' />
-                <Button as={Link} to='/login' size='huge' inverted>
-                    L0gIn
-                </Button>
+                {isLoggedIn && user ? (
+                    <Fragment>
+                        <Header as='h2' inverted content={`Welcome back ${user.displayName}`} />
+                        <Button as={Link} to='/activities' size='huge' inverted>
+                            Go to Activities...
+                        </Button>                        
+                    </Fragment>
+                ) : (
+                    <Fragment>
+                        <Header as='h2'color='green' content='Welcome to eActivities' />
+                        <Button as={Link} to='/login' size='huge' inverted>
+                            LoGIn
+                        </Button>
+                        <Button as={Link} to='/register' size='huge' inverted>
+                            ReGistration
+                        </Button>
+                    </Fragment>
+                )}
+
             </Container>
         </Segment>
     )
