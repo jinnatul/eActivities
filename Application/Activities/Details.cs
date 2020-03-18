67901f6128a35bcbb284913722aa079fd2256ed6
jinnatul <morolswediu@gmail.com>
2020-03-18 21:01:16 +0600

    adding DTO to shape data

--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -12,12 +12,12 @@ namespace Application.Activities
 {
     public class Details
     {
-        public class Query : IRequest<Activity> 
+        public class Query : IRequest<ActivityDto> 
         {
             public Guid Id { get; set; }
         }
 
-        public class Handler : IRequestHandler<Query, Activity>
+        public class Handler : IRequestHandler<Query, ActivityDto>
         {
             private readonly DataContext _context;
             public Handler(DataContext context)
@@ -25,7 +25,7 @@ namespace Application.Activities
                 _context = context;
             }
 
-            public async Task<Activity> Handle(Query request,
+            public async Task<ActivityDto> Handle(Query request,
             CancellationToken cancellationToken)
             {
                 var activity = await _context.Activities
@@ -39,7 +39,7 @@ namespace Application.Activities
                         new{activity="Not Found"}
                     );
 
-                return activity;
+                return new ActivityDto;
             }
         }
     }
