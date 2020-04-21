0058c651f3a9ff35bf19c6d652aeb8d67a689e35
jinnatul <morolswediu@gmail.com>
2020-04-21 20:48:06 +0600

    mapping profile configuration for user image

--- Application/User/Login.cs
+++ Application/User/Login.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
@@ -59,7 +60,7 @@ namespace Application.User
                         DisplayName = user.DisplayName,
                         Token = _jwtGenerator.CreateToken(user),
                         UserName = user.UserName,
-                        Image = null
+                        Image = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
                     };
                 }
 
