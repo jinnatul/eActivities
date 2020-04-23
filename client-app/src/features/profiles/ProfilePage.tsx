474770d2ccb4d02579de750aa488b6968102ca1b
jinnatul <morolswediu@gmail.com>
2020-04-23 13:19:59 +0600

    add profile content

--- client-app/src/features/profiles/ProfilePage.tsx
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Grid } from 'semantic-ui-react';
 import ProfileHeader from './ProfileHeader';
+import ProfileContent from './ProfileContent';
 
 const ProfilePage = () => {
     return (
         <Grid.Column width={16}>
             <ProfileHeader/>
+            <ProfileContent/>
         </Grid.Column>
     )
 }
