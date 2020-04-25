1bc3ad4a39404dd7564f082a3a9a85202fc44b39
jinnatul <morolswediu@gmail.com>
2020-04-26 00:05:29 +0600

    update profile client side

--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Tab } from 'semantic-ui-react';
 import ProfilePhotos from './ProfilePhotos';
-import { observer } from 'mobx-react-lite';
+import ProfileDescription from './ProfileDescription';
 
 const panes = [
-    {menuItem: 'About', render: () => <Tab.Pane>About Content</Tab.Pane>},
+    {menuItem: 'About', render: () => <ProfileDescription/>},
     {menuItem: 'Photos', render: () => <ProfilePhotos/>},
     {menuItem: 'Activities', render: () => <Tab.Pane>Activities Content</Tab.Pane>},
     {menuItem: 'Followers', render: () => <Tab.Pane>Followers Content</Tab.Pane>},
@@ -17,9 +17,8 @@ const ProfileContent = () => {
             menu={{fluid: true, vertical: true}}
             menuPosition="right"
             panes={panes}
-            activeIndex={1}
         />
     )
 }
 
-export default observer(ProfileContent)
+export default ProfileContent
