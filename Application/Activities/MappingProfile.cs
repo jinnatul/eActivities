a807adf71b160aa932bf10a507200e2bc80aef12
jinnatul <morolswediu@gmail.com>
2020-05-04 13:58:23 +0600

    custom value resolver automapper

--- Application/Activities/MappingProfile.cs
+++ Application/Activities/MappingProfile.cs
@@ -13,7 +13,8 @@ namespace Application.Activities
                 .ForMember(d => d.UserName, o => o.MapFrom(s => s.AppUser.UserName))
                 .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
                 .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos
-                    .FirstOrDefault(x => x.IsMain).Url));
+                    .FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(d => d.Following, o => o.MapFrom<FollowingResolver>());
         }
     }
 }
