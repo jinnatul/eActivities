037c1b01667000d37b9289f918291d83d65159ee
jinnatul <morolswediu@gmail.com>
2020-05-04 22:03:37 +0600

    adding follow and unfollow method

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -73,7 +73,9 @@ const Profiles = {
     uploadPhoto: (photo: Blob): Promise<IPhoto> => requests.postForm(`/photos`, photo),
     setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
     deletePhoto: (id: string) => requests.del(`/photos/${id}`),
-    updateProfile: (profile: Partial<IProfile>) => requests.put(`/profiles`, profile)
+    updateProfile: (profile: Partial<IProfile>) => requests.put(`/profiles`, profile),
+    follow: (username: string) => requests.post(`/profiles/${username}/follow`, {}),
+    unfollow: (username: string) => requests.del(`/profiles/${username}/follow`)
 }
 
 export default {
