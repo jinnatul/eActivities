9243fb7ac070235d56f915a58638940b80cc5d17
jinnatul <morolswediu@gmail.com>
2020-05-08 10:46:55 +0600

    user activity filter client

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -36,13 +36,18 @@ axios.interceptors.response.use(undefined, error => {
 const responseBody = (response: AxiosResponse) => response.data;
 
 const sleep = (ms: number) => (response: AxiosResponse) => 
-    new Promise<AxiosResponse>(resolve => setTimeout(() => resolve(response), ms));
+    new Promise<AxiosResponse>(resolve => setTimeout(() => 
+        resolve(response), ms));
 
 const requests = {
-    get: (url: string) => axios.get(url).then(sleep(1000)).then(responseBody),
-    post: (url: string, body: {}) => axios.post(url, body).then(sleep(1000)).then(responseBody),
-    put: (url: string, body: {}) => axios.put(url, body).then(sleep(1000)).then(responseBody),
-    del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody),
+    get: (url: string) => 
+        axios.get(url).then(sleep(1000)).then(responseBody),
+    post: (url: string, body: {}) => 
+        axios.post(url, body).then(sleep(1000)).then(responseBody),
+    put: (url: string, body: {}) => 
+        axios.put(url, body).then(sleep(1000)).then(responseBody),
+    del: (url: string) => 
+        axios.delete(url).then(sleep(1000)).then(responseBody),
     postForm: (url: string, file: Blob) => {
         let formData = new FormData();
         formData.append('File', file);
@@ -55,29 +60,48 @@ const requests = {
 const Activities = {
     list: (params: URLSearchParams): Promise<IActivitiesEnvelope> => 
         axios.get('/activities', {params: params}).then(sleep(1000)).then(responseBody),
-    details: (id: string) => requests.get(`/activities/${id}`),
-    create: (activity: IActivity) => requests.post('/activities', activity),
-    update: (activity: IActivity) => requests.put(`/activities/${activity.id}`, activity),
-    delete: (id: string) => requests.del(`/activities/${id}`),
-    attend: (id: string) => requests.post(`/activities/${id}/attend`, {}),
-    unattend: (id: string) => requests.del(`/activities/${id}/attend`)
+    details: (id: string) => 
+        requests.get(`/activities/${id}`),
+    create: (activity: IActivity) => 
+        requests.post('/activities', activity),
+    update: (activity: IActivity) => 
+        requests.put(`/activities/${activity.id}`, activity),
+    delete: (id: string) => 
+        requests.del(`/activities/${id}`),
+    attend: (id: string) => 
+        requests.post(`/activities/${id}/attend`, {}),
+    unattend: (id: string) => 
+        requests.del(`/activities/${id}/attend`)
 };
 
 const User = {
-    current: (): Promise<IUser> => requests.get('/user'),
-    login: (user: IUserFormValues): Promise<IUser> => requests.post(`/user/login`, user),
-    register: (user: IUserFormValues): Promise<IUser> => requests.post(`/user/register`, user)
+    current: (): Promise<IUser> => 
+        requests.get('/user'),
+    login: (user: IUserFormValues): Promise<IUser> => 
+        requests.post(`/user/login`, user),
+    register: (user: IUserFormValues): Promise<IUser> => 
+        requests.post(`/user/register`, user)
 };
 
 const Profiles = {
-    get: (username: string): Promise<IProfile> => requests.get(`/profiles/${username}`),
-    uploadPhoto: (photo: Blob): Promise<IPhoto> => requests.postForm(`/photos`, photo),
-    setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
-    deletePhoto: (id: string) => requests.del(`/photos/${id}`),
-    updateProfile: (profile: Partial<IProfile>) => requests.put(`/profiles`, profile),
-    follow: (username: string) => requests.post(`/profiles/${username}/follow`, {}),
-    unfollow: (username: string) => requests.del(`/profiles/${username}/follow`),
-    listFollowings: (username: string, predicate: string) => requests.get(`/profiles/${username}/follow?predicate=${predicate}`)
+    get: (username: string): Promise<IProfile> => 
+        requests.get(`/profiles/${username}`),
+    uploadPhoto: (photo: Blob): Promise<IPhoto> => 
+        requests.postForm(`/photos`, photo),
+    setMainPhoto: (id: string) => 
+        requests.post(`/photos/${id}/setMain`, {}),
+    deletePhoto: (id: string) => 
+        requests.del(`/photos/${id}`),
+    updateProfile: (profile: Partial<IProfile>) => 
+        requests.put(`/profiles`, profile),
+    follow: (username: string) => 
+        requests.post(`/profiles/${username}/follow`, {}),
+    unfollow: (username: string) => 
+        requests.del(`/profiles/${username}/follow`),
+    listFollowings: (username: string, predicate: string) => 
+        requests.get(`/profiles/${username}/follow?predicate=${predicate}`),
+    listActivities: (username: string, predicate: string) => 
+        requests.get(`/profiles/${username}/activities?predicate=${predicate}`)
 }
 
 export default {
